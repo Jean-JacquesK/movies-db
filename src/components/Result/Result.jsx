ae77c039b12fc1ba4799d35be9dbc20d083d9235
Jean-JacquesK <jj.kechichian@gmail.com>
2023-03-21 17:53:03 +0100

    New display for result

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import "./result.css";
 
 function Result({ item }) {
+  const [onFlip, setOnFlip] = useState(false);
+
+  function handleClick() {
+    setOnFlip(!onFlip);
+  }
+
   return (
-    <div className='result-items'>
-      <img
-        className='result-img'
-        src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-        alt=''
-      />
-      <div className='result-details'>
-        <h3 className='result-title'>{item.original_title}</h3>
-        <p>{item.overview}</p>
-      </div>
+    <div
+      className={!onFlip ? "result-items" : "result-items back"}
+      onClick={handleClick}
+    >
+      {!onFlip && (
+        <div className='item-box'>
+          <h3 className='result-title'>{item.original_title}</h3>
+          <img
+            className='result-img'
+            src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+            alt=''
+          />
+        </div>
+      )}
+
+      {onFlip && (
+        <div className='result-details'>
+          <h4>{item.title}</h4>
+          <p>{item.overview}</p>
+          <p>Completion date : {item.release_date}</p>
+          <p>Average note : {item.vote_average} </p>
+        </div>
+      )}
     </div>
   );
 }
