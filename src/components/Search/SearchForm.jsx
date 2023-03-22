6e1c6b706cd74c82901e251d3831929fd003cec2
Jean-JacquesK <jj.kechichian@gmail.com>
2023-03-22 15:44:11 +0100

    css mobile first

--- src/components/Search/SearchForm.jsx
+++ src/components/Search/SearchForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { fetchSearchApi } from "../../utils/fetchApi";
 
+import "./search.css";
+
 function SearchForm({ setMovies, setSearchParams }) {
   const [search, setSearch] = useState("");
 
