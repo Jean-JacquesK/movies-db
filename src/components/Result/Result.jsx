56baa30b4208152a198d8247f6127d109172850b
Jean-JacquesK <jj.kechichian@gmail.com>
2023-03-28 13:42:15 +0200

    card hover css

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,37 +1,23 @@
-import { useState } from "react";
 import "./result.css";
 
 function Result({ item }) {
-  const [onFlip, setOnFlip] = useState(false);
-
-  function handleClick() {
-    setOnFlip(!onFlip);
-  }
-
   return (
-    <div
-      className={!onFlip ? "result-items" : "result-items back"}
-      onClick={handleClick}
-    >
-      {!onFlip && (
-        <div className='item-box'>
-          <h3 className='result-title'>{item.title}</h3>
-          <img
-            className='result-img'
-            src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-            alt={`${item.title} picture`}
-          />
-        </div>
-      )}
+    <div className='result-items'>
+      <div className='item-box front'>
+        <h3 className='result-title'>{item.title}</h3>
+        <img
+          className='result-img'
+          src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+          alt={`${item.title} picture`}
+        />
+      </div>
 
-      {onFlip && (
-        <div className='result-details'>
-          <h4 className='result-title'>{item.title}</h4>
-          <p>{item.overview}</p>
-          <p>Completion date : {item.release_date}</p>
-          <p>Average note : {item.vote_average} </p>
-        </div>
-      )}
+      <div className='item-box back'>
+        <h4 className='result-title'>{item.title}</h4>
+        <p className='result-para'>{item.overview}</p>
+        <p>Completion date : {item.release_date}</p>
+        <p>Average note : {item.vote_average} </p>
+      </div>
     </div>
   );
 }
